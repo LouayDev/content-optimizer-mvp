df9d2e702f1a635dd5c009356fcee8562fb20d37
Louai Amrouche <61905516+LouayDev@users.noreply.github.com>
2024-09-20 00:05:14 +0100

    creating the signup route and including the zod validation to it imported from shared/schemas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 
+const userSchema = require('../shared/schemas/user.js')
+const z = require('zod')
+
+//middleware
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('hello there')
+})
+
+app.post('/signup', async (req, res) => {
+  const {username, password} = req.body
+  const result = userSchema.safeParse({username, password})
+
+  if(!result.success){ 
+    return res.status(400).json({message: result.error.issues[0].message})
+  }
+
+})
+
 const PORT = 4000
 app.listen(PORT, () => {
   console.log('server running at port:', PORT)
