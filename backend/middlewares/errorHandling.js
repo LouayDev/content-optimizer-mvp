08dfa2e8e7a88509051efc867bd435803a427aae
Louai Amrouche <61905516+LouayDev@users.noreply.github.com>
2024-09-22 21:08:30 +0100

    adding the file that was missed in the previous commit

---
+++ backend/middlewares/errorHandling.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const invalidJsonInBody = () => (err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    res.status(400).json({ message: "ivalid json in the body" });
+  }
+  next();
+};
+
+module.exports = { invalidJsonInBody };
