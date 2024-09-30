f73b92d26f3f63a9e4bc3fd2414fb76c8dd7345b
Louai Amrouche <61905516+LouayDev@users.noreply.github.com>
2024-09-30 18:30:28 +0100

    create: function getAverageWordCount, it gives the average word count of the top 3 or so given urls by lpping over the provided urls and calling getPageWordCount() function for each url, then summing them, deviding them by the total urls provided and flooring them

--- backend/router/businessLogic.js
+++ backend/router/businessLogic.js
@@ -10,33 +10,8 @@ const sanitizeHTML = require("sanitize-html");
 const { isValidURL } = require("../utils/isValidUrl");
 const { removeWhiteSpaces } = require("../utils/removeWhiteSpaces");
 
-//getting the average wordcount for the top 3 websites
-const getPageWordCount = async (url) => {
-  if (!isValidURL(url)) {
-    throw new Error("url argument must be a valid url");
-  }
-
-  const totalWordCount = 0;
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  try {
-    await page.goto(url, { waitUntil: "domcontentloaded" });
-    const textContent = await page.evaluate(() => {
-      return document.body.innerText;
-    });
-    const wordCount = textContent.split(/\s+/).length;
-    console.log(`word count for ${url} is: `, wordCount);
-    await browser.close();
-  } catch (err) {
-    throw err;
-  }
-
-  return totalWordCount / 3;
-};
-
 router.post("/fetch-html", authenticate, async (req, res) => {
-  const { url } = req.body;
+  const { url, topic: query } = req.body;
 
   //checking if url exists & its valid
   if (!url) {
@@ -106,9 +81,12 @@ router.post("/fetch-html", authenticate, async (req, res) => {
     //closing the browser and returning the response
     await browser.close();
 
+    const TopThreeUrls = await getTopThreeWebsitesURL(query);
+    const averageWordCount = await getAverageWordCount(TopThreeUrls);
+
     return res.json({
       html: neetHTML,
-      // target_wordcount:
+      target_wordcount: averageWordCount,
     });
   } catch (err) {
     console.log(err);
@@ -120,8 +98,6 @@ module.exports = router;
 
 //retrieving the top three urls for a given query
 const getTopThreeWebsitesURL = async (query) => {
-  let topThreeUrls = [];
-
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -134,8 +110,45 @@ const getTopThreeWebsitesURL = async (query) => {
     const topThreeLinks = await page.$$eval("a h3", (anchors) => {
       return anchors.slice(0, 3).map((anchor) => anchor.closest("a").href);
     });
+
+    await browser.close();
+    return topThreeLinks;
   } catch (err) {
     console.log("getTopThreeWebsitesURL err: ", err);
+    throw err;
+  }
+};
+
+//getting the top three urls
+const getAverageWordCount = async (urls) => {
+  let totalWordCount = 0;
+  for (let url of urls) {
+    const wordCount = await getPageWordCount(url);
+    totalWordCount += wordCount;
+  }
+  console.log("total word count for the top 3 sites is: ", totalWordCount);
+  return Math.floor(totalWordCount / urls.length);
+};
+
+//getting the wordcount for a given page
+const getPageWordCount = async (url) => {
+  if (!isValidURL(url)) {
+    throw new Error("url argument must be a valid url");
+  }
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  try {
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    const textContent = await page.evaluate(() => {
+      return document.body.innerText;
+    });
+    const wordCount = textContent.split(/\s+/).length;
+    console.log(`word count for ${url} is: `, wordCount);
+    await browser.close();
+    return wordCount;
+  } catch (err) {
     throw err;
   }
 };
